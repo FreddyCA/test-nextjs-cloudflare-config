cb6e5c7227a1cfc8161f78896bf2ba081b469832
Freddy Coaricona <fcoaricona@outlook.com>
2024-07-15 02:33:25 -0400

    feat: change Table Users, install bcrypt

--- app/lib/db/schema.ts
+++ app/lib/db/schema.ts
@@ -1,7 +1,22 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-export const usersTable = sqliteTable("users", {
+// export const usersTable = sqliteTable("users", {
+//   id: integer("id").primaryKey(),
+//   name: text("name").notNull(),
+//   age: integer("age").notNull(),
+//   email: text("email").unique().notNull(),
+// });
+
+export const messagesTable = sqliteTable("messages", {
   id: integer("id").primaryKey(),
+  message: text("name").notNull(),
+});
+
+// SEMBRANDO EL FORMATO DE LA BASE DE DATOS
+// USERS
+
+export const usersTable = sqliteTable("users", {
+  id: text("id").primaryKey().default(`uuid_generate_v4()`),
   name: text("name").notNull(),
-  age: integer("age").notNull(),
   email: text("email").unique().notNull(),
+  password: text("password").notNull(),
 });
---
+++ migrations/0001_wonderful_strong_guy.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `messages` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text NOT NULL
+);
