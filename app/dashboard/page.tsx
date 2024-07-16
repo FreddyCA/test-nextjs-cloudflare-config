1ee1d3d1601dd3b8df2a029d3756274564e59385
Freddy Coaricona <fcoaricona@outlook.com>
2024-07-16 15:26:35 -0400

    feat: add UI skeletons

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+import { Suspense } from "react";
 import { fetchCardData } from "../lib/data";
 import CardWrapper from "../ui/dashboard/cards";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "../ui/skeletons";
 
 export const runtime = "edge";
 
@@ -13,11 +19,18 @@ export default function DashboardPage() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <CardWrapper />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart />
-        <LatestInvoices />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+          <RevenueChart />
+        </Suspense>
       </div>
     </main>
   );
