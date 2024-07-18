ea6b433a36f41a8e620a623f41f55132066ef124
Freddy Coaricona <fcoaricona@outlook.com>
2024-07-17 20:16:21 -0400

    refactor change FormSchema

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,9 +31,15 @@ export async function deleteInvoice(id: string) {
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string().nonempty("Please select a customer."),
-  amount: z.coerce.number().gt(0, { message: "Please enter an amount greater than $0." }),
-  status: z.enum(["pending", "paid"], { invalid_type_error: "Please select an invoice status" }),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 });
 
