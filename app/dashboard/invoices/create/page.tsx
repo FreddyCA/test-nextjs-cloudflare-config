fc77f54d676e81e3e6b9b6122a1b2d12fe031e1e
Freddy Coaricona <fcoaricona@outlook.com>
2024-07-18 23:51:52 -0400

    refactor: add runtime edge create invoice

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -3,12 +3,13 @@ import { fetchCustomers } from "@/app/lib/data";
 import FormInvoice from "@/app/ui/invoices/create-form";
 import { Metadata } from "next";
 
- 
+export const runtime = 'edge';
+
 export const metadata: Metadata = {
   title: 'Invoices Create',
 };
-export default async function Page() {
-  const customers = await fetchCustomers();
+export default async function InvoicePage() {
+  const responseCustomers = await fetchCustomers();
  
   return (
     <main>
@@ -22,7 +23,7 @@ export default async function Page() {
           },
         ]}
       /> */}
-      <FormInvoice customers={customers} />
+      <FormInvoice customers={responseCustomers} />
     </main>
   );
 }
